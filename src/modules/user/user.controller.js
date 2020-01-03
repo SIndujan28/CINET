ff8eb31cab179fc6be3e6b316782fe34a215cb66
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-01-03 13:53:42 +0530

    add user list,update,delete details

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,4 +1,5 @@
 import HTTPStatus from 'http-status';
+import _ from 'lodash';
 import User from './user.model';
 
 export async function signup(req, res) {
@@ -13,4 +14,51 @@ export async function signup(req, res) {
 export async function login(req, res, next) {
   res.status(HTTPStatus.OK).json(req.user);
   next();
+}
+
+export async function list(req, res) {
+  try {
+    const users = await User.find({}, 'email firstName createdAt updatedAt');
+    return res.status(HTTPStatus.OK).json(users);
+  } catch (error) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
+  }
+}
+
+export async function userById(req, res, next, id) {
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(HTTPStatus.NOT_FOUND).json('Not found');
+    }
+    req.profile = user;
+    next();
+  } catch (error) {
+    return res.status(HTTPStatus.BAD_REQUEST).json('User not found');
+  }
+}
+export async function read(req, res) {
+  req.profile.password = undefined;
+  return res.status(HTTPStatus.OK).json(req.profile);
+}
+
+export async function remove(req, res) {
+  try {
+    const user = req.profile;
+    await user.remove();
+    return res.status(HTTPStatus.OK).json('User deleted');
+  } catch (error) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
+  }
+}
+
+export async function update(req, res) {
+  try {
+    let user = req.profile;
+    user = _.extend(user, req.body);
+    await user.save();
+    return res.status(HTTPStatus.ACCEPTED).json(user);
+  } catch (error) {
+    return res.status(HTTPStatus.BAD_REQUEST).json(error);
+  }
 }
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -5,7 +5,12 @@ import { authLocal } from './../../services/auth.service';
 
 const routes = new Router();
 
+routes.param('userId', userController.userById);
 routes.post('/signup', userController.signup);
 routes.post('/login', authLocal, userController.login);
+routes.get('/list', userController.list);
+routes.get('/:userId', userController.read);
+routes.delete('/:userId', userController.remove);
+routes.put('/:userId', userController.update);
 export default routes;
 
