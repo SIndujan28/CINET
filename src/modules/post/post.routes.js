1b08875efaa7df7e6d892ae66a49b38508bdf3c5
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-01-07 19:38:02 +0530

    add requireSignin feature

--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -1,19 +1,20 @@
 import { Router } from 'express';
 import * as postController from './post.controller';
 import { userById } from './../user/user.controller';
+import { requireSignin } from './../../services/valid.service';
 
 const routes = new Router();
 
 routes.param('userId', userById);
 routes.param('postId', postController.postById);
-routes.get('/feed/:userId', postController.listNewsFeed);
-routes.get('/by/:userId', postController.listByUser);
+routes.get('/feed/:userId', requireSignin, postController.listNewsFeed);
+routes.get('/by/:userId', requireSignin, postController.listByUser);
 routes.get('/photo/:postId', postController.photo);
-routes.delete('/:postId', postController.checkPoster, postController.remove);
-routes.put('/new/:userId', postController.create);
-routes.put('/like', postController.like);
-routes.put('/unlike', postController.unlike);
-routes.put('/comment', postController.addComment);
-routes.put('/uncomment', postController.removeComment);
+routes.delete('/:postId', requireSignin, postController.checkPoster, postController.remove);
+routes.put('/new/:userId', requireSignin, postController.create);
+routes.put('/like', requireSignin, postController.like);
+routes.put('/unlike', requireSignin, postController.unlike);
+routes.put('/comment', requireSignin, postController.addComment);
+routes.put('/uncomment', requireSignin, postController.removeComment);
 
 export default routes;
